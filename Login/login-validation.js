c61e7812a5742242e43017b2c8876478c23ca838
AchiGel <ar.gelashvili@gmail.com>
2023-12-22 14:55:15 +0400

    handle with login form and localStorage for categories

--- Login/login-validation.js
+++ Login/login-validation.js
@@ -1,37 +1,102 @@
 const loginBtn = document.querySelector("#loginBtn");
 const loginForm = document.querySelector(".login");
+const formBtn = loginForm.querySelector("button");
 const loginInput = document.querySelector("#email");
 const worningMessage = document.querySelector("#warningMessage");
 const closeBtn = document.querySelector(".closeBtn");
 const modal = document.querySelector(".modal");
 
-loginBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+const successfulLogin = `<form>
+      <img class="closeBtn" src="https://svgshare.com/getbyhash/sha1-PRNSwMCM+7R81r9dgj+3jiuCnRQ=" alt="" />
+      <img style="margin-bottom:16px;" src="https://svgshare.com/getbyhash/sha1-lfe7SPACmJyBUAJJtxmkqY0U/7g=" alt="" />
+      <h2 style="margin-bottom:48px;">წარმატებული ავტორიზაცია</h2>
+      <button class="loginOk" type="submit">კარგი</button>
+</form>`;
+
+// --------------------------------------------------------- //
+
+// Function to handle display of warning messages
+function displayWarning() {
+  worningMessage.style.display = "flex";
+  loginInput.style.marginBottom = "8px";
+  loginInput.style.border = "1px solid #EA1919";
+  loginInput.style.background = "#FAF2F3";
+}
+
+// Function to reset input styling and hide warning
+function resetInputStyling() {
+  worningMessage.style.display = "none";
+  loginInput.style.marginBottom = "24px";
+  loginInput.style.border = "";
+  loginInput.style.background = "";
+}
+
+// Function to handle successful login
+function handleSuccessfulLogin() {
+  loginForm.innerHTML = successfulLogin;
+  loginBtn.innerText = "დაამატე ბლოგი";
+}
+
+loginForm.addEventListener("submit", async function checkUser(e) {
+  try {
+    e.preventDefault();
+    const enteredEmail = loginInput.value;
+
+    const token =
+      "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
+
+    const response = await fetch(
+      "https://api.blog.redberryinternship.ge/api/login",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          email: enteredEmail,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      displayWarning();
+    } else {
+      handleSuccessfulLogin();
+    }
+  } catch (error) {
+    console.log("Error occurred:", error);
+    // Handle error here
+  }
 });
 
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", (event) => {
-  if (event.target == modal) {
+// Event delegation for modal close
+document.addEventListener("click", (event) => {
+  if (
+    event.target === modal ||
+    event.target.classList.contains("closeBtn") ||
+    event.target.classList.contains("loginOk")
+  ) {
     modal.style.display = "none";
   }
 });
 
-loginInput.addEventListener("input", () => {
+loginInput.addEventListener("input", (e) => {
+  e.preventDefault();
   if (
-    !loginInput.value.trim().includes("@redberry.ge") ||
+    !loginInput.value.trim().endsWith("@redberry.ge") ||
     !(loginInput.value.trim().length > 13)
   ) {
-    worningMessage.style.display = "flex";
-    loginInput.style.marginBottom = "8px";
-    loginInput.style.border = "1px solid #EA1919";
-    loginInput.style.background = "#FAF2F3";
+    displayWarning();
   } else {
-    worningMessage.style.display = "none";
-    loginInput.style.marginBottom = "24px";
-    loginInput.style.border = "";
-    loginInput.style.background = "";
+    resetInputStyling();
+  }
+});
+
+loginBtn.addEventListener("click", () => {
+  if (loginBtn.innerText === "შესვლა") modal.style.display = "block";
+  if (loginBtn.innerText === "დაამატე ბლოგი") {
+    window.location.href = "../Add_blog/add_blog.html";
   }
 });
--- main.js
+++ main.js
@@ -1,42 +1,64 @@
 const categoriesList = document.querySelector(".categoriesList");
+const listItems = categoriesList.children;
 
-// ------------ Get categories from API ---------------//
-
-async function getCategories() {
+async function loadCategories() {
   try {
-    const token =
-      "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
-    const response = await fetch(
-      "https://api.blog.redberryinternship.ge/api/categories",
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${token}`,
-        },
+    let categories = JSON.parse(localStorage.getItem("categories"));
+    if (!categories) {
+      const token =
+        "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
+      const response = await fetch(
+        "https://api.blog.redberryinternship.ge/api/categories",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
       }
-    );
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+      const dataJson = await response.json();
+      categories = dataJson.data;
+
+      localStorage.setItem("categories", JSON.stringify(categories));
     }
 
-    const dataJson = await response.json();
-
-    const categories = await dataJson.data;
-
-    for (let i = 0; i < categories.length; i++) {
-      let categoryItem = await categories[i];
-      let categoryNode = `<li style = "background-color: ${
-        categoryItem.background_color + "14"
-      }; color: ${categoryItem.background_color}">${categoryItem.title}</li>`;
-      categoriesList.innerHTML += categoryNode;
-    }
+    displayCategories(categories);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
   }
 }
 
-getCategories();
+function displayCategories(categories) {
+  categories.forEach((element) => {
+    let li = document.createElement("li");
+    li.textContent = element.title;
+    li.style.backgroundColor = element.background_color + "14";
+    li.style.color = element.background_color;
 
-// ------------ Get categories from API ---------------//
+    if (localStorage.getItem(`chosen_${element.title}`) === "true") {
+      li.classList.add("chosenLi");
+    }
+
+    categoriesList.appendChild(li);
+
+    li.addEventListener("click", (e) => {
+      e.preventDefault();
+      li.classList.toggle("chosenLi");
+
+      localStorage.setItem(
+        `chosen_${element.title}`,
+        li.classList.contains("chosenLi")
+      );
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadCategories();
+});
