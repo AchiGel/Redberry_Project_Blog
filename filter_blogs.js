82a9aecca839174dea3fc2aff40e093ce638b3a1
AchiGel <ar.gelashvili@gmail.com>
2023-12-27 14:17:19 +0400

    filtering blogs on main page

--- filter_blogs.js
+++ filter_blogs.js
@@ -1,64 +1,136 @@
 const blogsArea = document.querySelector("#blogsArea");
-const blogCategory = document.querySelectorAll(".blogs-categories");
 const categories = document.querySelector(".categoriesList");
-const listItem = categories.children;
 
 async function getBlogs() {
   try {
-    const token =
-      "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
-    const response = await fetch(
-      "https://api.blog.redberryinternship.ge/api/blogs",
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const allBlogs = await fetchBlogsFromAPI();
+    const selectedCategories = getSelectedCategoriesFromLocalStorage();
 
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
+    let filteredBlogs = filterBlogsByCategories(allBlogs, selectedCategories);
 
-    const dataJson = await response.json();
-    const blogs = await dataJson.data;
+    // Clear existing blogs before displaying new ones
+    clearBlogs();
 
-    blogs.map((items) => {
-      let categoriesListForBlog = document.createElement("ul");
-      categoriesListForBlog.classList.add("categoriesList");
-      categoriesListForBlog.classList.add("categoriesListForBlog");
-      for (let cat of items.categories) {
-        let category = document.createElement("li");
-        category.textContent = cat.title;
-        category.style.color = cat.background_color;
-        category.style.backgroundColor = cat.background_color + "14";
-        categoriesListForBlog.appendChild(category);
-      }
-
-      const blogPost = document.createElement("div");
-      blogPost.classList.add("blogPost");
-      let blogInner = `<img class="blog-photo" src="${items.image}" alt="" />
-          <h4>${items.author}</h4>
-          <h6>${items.publish_date}</h6>
-          <h2>${items.title}</h2>
-          <div class="blogs-categories"></div>
-          <p>
-            ${items.description}
-          </p>
-          <a href=""
-            >სრულად ნახვა
-            <img class="arrowLogo" src="./assets/images/Arrow.png" alt=""
-          /></a>`;
-      blogPost.innerHTML = blogInner;
-      let categoriesContainer = blogPost.querySelector(".blogs-categories");
-      categoriesContainer.appendChild(categoriesListForBlog);
-      blogsArea.append(blogPost);
-    });
+    displayBlogs(filteredBlogs);
   } catch (error) {
-    console.log(error);
+    handleGetBlogsError(error);
   }
 }
 
-window.addEventListener("DOMContentLoaded", getBlogs());
+function clearBlogs() {
+  // Remove all existing blogs from the DOM
+  blogsArea.innerHTML = "";
+}
+
+async function fetchBlogsFromAPI() {
+  const token =
+    "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
+  const response = await fetch(
+    "https://api.blog.redberryinternship.ge/api/blogs",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
+
+  const dataJson = await response.json();
+  return dataJson.data;
+}
+
+function getSelectedCategoriesFromLocalStorage() {
+  const localStorageKeys = Object.keys(localStorage);
+  const chosenCategories = localStorageKeys.filter((key) =>
+    key.startsWith("chosen_")
+  );
+  return chosenCategories
+    .filter((key) => localStorage.getItem(key) === "true")
+    .map((key) => key.replace("chosen_", ""));
+}
+
+function filterBlogsByCategories(allBlogs, selectedCategories) {
+  if (selectedCategories.length === 0) {
+    return allBlogs; // Return all blogs if no categories are selected
+  }
+
+  return allBlogs.filter((blog) =>
+    selectedCategories.every((selectedCategory) =>
+      blog.categories.some(
+        (blogCategory) => selectedCategory === blogCategory.title
+      )
+    )
+  );
+}
+
+function displayBlogs(blogs) {
+  blogs.forEach((blog) => {
+    const categoriesListForBlog = createCategoriesListForBlog(blog.categories);
+    const blogTextContent = blog.description.slice(0, 100) + "...";
+    const blogPost = createBlogPostElement(
+      blog,
+      categoriesListForBlog,
+      blogTextContent
+    );
+    blogsArea.appendChild(blogPost);
+  });
+}
+
+function createCategoriesListForBlog(categories) {
+  const categoriesListForBlog = document.createElement("ul");
+  categoriesListForBlog.classList.add("categoriesListForBlog");
+
+  categories.forEach((cat) => {
+    const category = document.createElement("li");
+    category.textContent = cat.title;
+    category.style.color = cat.background_color;
+    category.style.backgroundColor = cat.background_color + "14";
+    category.style.listStyle = "none";
+    category.style.fontSize = "12px";
+    category.style.padding = "6px 10px";
+    category.style.borderRadius = "30px";
+    categoriesListForBlog.appendChild(category);
+  });
+
+  return categoriesListForBlog;
+}
+
+function createBlogPostElement(blog, categoriesListForBlog, blogText) {
+  const blogPost = document.createElement("div");
+  blogPost.classList.add("blogPost");
+
+  blogPost.innerHTML = `
+    <img class="blog-photo" src="${blog.image}" alt="" />
+    <h4>${blog.author}</h4>
+    <h6>${blog.publish_date}</h6>
+    <h2>${blog.title}</h2>
+    <div class="blogs-categories"></div>
+    <p class="blogs-text"></p>
+    <a href="#">სრულად ნახვა<img class="arrowLogo" src="./assets/images/Arrow.png" alt="" /></a>
+  `;
+
+  const categoriesContainer = blogPost.querySelector(".blogs-categories");
+  categoriesContainer.appendChild(categoriesListForBlog);
+
+  const blogsText = blogPost.querySelector(".blogs-text");
+  blogsText.textContent = blogText;
+
+  return blogPost;
+}
+
+function handleGetBlogsError(error) {
+  console.error(error);
+  // Additional error handling logic if needed
+}
+
+// Example: Trigger getBlogs when categories change
+categories.addEventListener("change", getBlogs);
+
+window.addEventListener("DOMContentLoaded", () => {
+  getBlogs();
+});
--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 const categoriesList = document.querySelector(".categoriesList");
-const listItems = categoriesList.children;
+const TOKEN =
+  "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
+const STORAGE_KEY = "categories";
 
 async function loadCategories() {
   try {
-    let categories = JSON.parse(localStorage.getItem("categories"));
-    if (!categories) {
-      const token =
-        "9a18c528c3e35ec5e8d3497d24f2cf5e9029a874227ebb98261fe1cb4bf32c58";
+    let categories = localStorage.getItem(STORAGE_KEY);
+
+    if (categories) {
+      categories = JSON.parse(categories);
+    } else {
       const response = await fetch(
         "https://api.blog.redberryinternship.ge/api/categories",
         {
           method: "GET",
           headers: {
             Accept: "application/json",
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${TOKEN}`,
           },
         }
       );
@@ -25,40 +28,54 @@ async function loadCategories() {
       const dataJson = await response.json();
       categories = dataJson.data;
 
-      localStorage.setItem("categories", JSON.stringify(categories));
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(categories));
     }
 
     displayCategories(categories);
   } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
+    console.error("Error fetching categories:", error);
   }
 }
 
-function displayCategories(categories) {
-  categories.forEach((element) => {
-    let li = document.createElement("li");
-    li.textContent = element.title;
-    li.style.backgroundColor = element.background_color + "14";
-    li.style.color = element.background_color;
+function createListItem(element) {
+  const li = document.createElement("li");
+  li.textContent = element.title;
+  li.style.backgroundColor = element.background_color + "14";
+  li.style.color = element.background_color;
 
-    if (localStorage.getItem(`chosen_${element.title}`) === "true") {
-      li.classList.add("chosenLi");
-    }
+  if (localStorage.getItem(`chosen_${element.title}`) === "true") {
+    li.classList.add("chosenLi");
+  }
 
-    categoriesList.appendChild(li);
-
-    li.addEventListener("click", (e) => {
-      e.preventDefault();
-      li.classList.toggle("chosenLi");
-
-      localStorage.setItem(
-        `chosen_${element.title}`,
-        li.classList.contains("chosenLi")
-      );
-    });
-  });
+  li.addEventListener("click", toggleChosen);
+  return li;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadCategories();
+function toggleChosen(e) {
+  e.preventDefault();
+  const li = e.target;
+  li.classList.toggle("chosenLi");
+
+  const categoryName = li.textContent;
+  const isSelected = li.classList.contains("chosenLi");
+  localStorage.setItem(`chosen_${categoryName}`, isSelected);
+
+  // After selecting categories, filter blogs based on the chosen categories
+  getBlogs();
+}
+
+function displayCategories(categories) {
+  const fragment = document.createDocumentFragment();
+  categories.forEach((element) => {
+    const li = createListItem(element);
+    fragment.appendChild(li);
+  });
+  categoriesList.appendChild(fragment);
+}
+
+document.addEventListener("DOMContentLoaded", loadCategories);
+categoriesList.addEventListener("click", (e) => {
+  if (e.target.tagName === "li") {
+    toggleChosen(e);
+  }
 });
